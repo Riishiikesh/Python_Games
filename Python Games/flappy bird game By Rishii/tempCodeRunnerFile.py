a90783734cc2649d96579a4a0742a9c655d5dc86
Rishikesh Kumar <97401337+igriishii@users.noreply.github.com>
2024-09-20 03:20:03 +0530

    Add files via upload

---
+++ Python Games/flappy bird game By Rishii/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+import random # For generating random numbers
+import sys # We will use sys.exit to exit the program
+import pygame
+from pygame.locals import * # Basic pygame imports
+
+# Global Variables for the game
